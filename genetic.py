75fb65fdbd9794a2955ab79644900eb6e4ed20eb
Johan Burke <jburke4126@gmail.com>
2019-08-15 17:29:40 -0400

    Refactored genetic algorithm to work with measure data structure.  Still need to allow for midi output

--- genetic.py
+++ genetic.py
@@ -1,14 +1,15 @@
 from debug import debug_print
 import random
 from measure import Measure
+from decimal import Decimal
 import math
 
 
 def initial_population(population_count, num_measures=1):
-    random.seed()
+	random.seed()
 	# population is represented as a list of lists where the inner lists are each a list of measures (which represents one gene)
 	# TODO: add parameters for random_measure from user options
-    return [[Measure.random_measure() for _ in range(num_measures)] for _ in range(population_count)]
+	return [[Measure.random_measure() for _ in range(num_measures)] for _ in range(population_count)]
 
 
 # fitness is calculated as follows:
@@ -23,90 +24,93 @@ def fitness(gene, measurement_funcs=Measure.DEFAULT_MEASUREMENTS, measurement_ta
 	if len(measurement_funcs) != len(measurement_targets):
 		raise ValueError("The same number of measurement functions and target values must be provided.")
 	if measurement_weights is None:
-		measurement_weights = [1 for _ in range(len(measurement_funcs))]
+		measurement_weights = [Decimal(1) for _ in range(len(measurement_funcs))]
 	if len(measurement_weights) != len(measurement_funcs):
 		raise ValueError("The same number of measurement weights and measurement functions must be provided.")
-	result = 0
+	result = Decimal(0)
 	# TODO: allow for measurements that take into account neighboring measures
-    for measure in gene:
+	for measure in gene:
 		for i in range(len(measurement_funcs)):
-			measure_val = measurement_funcs[i](measure)
-			result -= math.abs(measure_val - measurement_targets[i]) * measurement_weights[i]
-    return result
+			measure_val = Decimal(measurement_funcs[i](measure))
+			result -= abs(measure_val - measurement_targets[i]) * measurement_weights[i]
+	return result
 
 
 def selection(population):
-    if not population:
-        return []
-    # TODO: allow different options for the fitness function
-    fitnesses = [fitness(x) for x in population]
-    mean_fitness = sum(fitnesses) / len(fitnesses)
-    selected_population = []
-    for i in range(len(population)):
-        proportion = fitnesses[i] // mean_fitness
-        for _ in range(int(proportion)):
-            selected_population.append(population[i])
-    diff_len = len(population) - len(selected_population)
-    for _ in range(diff_len):
-        selected_population.append(random.choice(population))
-    return selected_population
+	if not population:
+		return []
+	# TODO: allow different options for the fitness function
+	fitnesses = [fitness(x) for x in population]
+	# min_fitness = min(fitnesses)
+	# fitnesses = list(map(lambda fitness: fitness - min_fitness, fitnesses))
+	# we use truncation selection for now
+	selected = sorted(enumerate(fitnesses), key=lambda x: x[1])
+	num_selected = len(selected)
+	result = []
+	for i in range(num_selected // 2, num_selected):
+		result.append(population[selected[i][0]])
+	return result
 
 
 # granularity is the power of 2 division that the crossover point will be chosen at
 def crossover(population, percentage, granularity=3):
-    mates = [x for x in population if random.random() < percentage]
-    result = []
-    if len(mates) % 2 == 1:
-        mates.append(random.choice(population))
-    for i in range(0, len(mates), 2):
-        crossover_point = random.randint(0, 2 ** granularity)
-		crossover_point *= 1 / (2 ** granularity)
+	mates = random.sample(population, int(len(population) * percentage))
+	result = []
+	if len(mates) % 2 == 1:
+		mates.append(random.choice(population))
+	for i in range(0, len(mates), 2):
+		crossover_point = Decimal(random.randint(0, 2 ** granularity))
+		crossover_point *= Decimal(1 / (2 ** granularity))
 		first = []
 		second = []
 		for measure_index in range(len(population[i])):
 			first_split = population[i][measure_index].split(crossover_point)
 			second_split = population[i + 1][measure_index].split(crossover_point)
-			first.append(first_split[0] + second_split[1])
-			second.append(first_split[1] + second_split[0])
-        result.append(first)
-        result.append(second)
-		# TODO: add a step that fixes the resulting measures so that they are valid
-    return result
+			first_measure = Measure(first_split[0] + second_split[1])
+			first_measure.fix()
+			first.append(first_measure)
+			second_measure = Measure(first_split[1] + second_split[0])
+			second_measure.fix()
+			second.append(second_measure) 
+		result.append(first)
+		result.append(second)
+	return result
 
 
-# TODO: code below this line still needs to be refactored
+# creates a new population with the given percentage of notes mutated
+# to have their midi note numbers changed
+# the change is calculated from a gaussian distribution centered around the old note number
 def mutate(population, percentage):
-    result = []
-    for x in population:
-        mutations = 0
-        for i in range(GENE_BIT_COUNT):
-            mutations |= (random.random() < percentage) << i
-        result.append(x ^ mutations)
-    return result
+	result = []
+	for gene in population:
+		new_gene = []
+		for measure in gene:
+			new_notes = []
+			for note in measure.notes:
+				new_note = note
+				if random.random() < percentage:
+					# for now we vary the note number since
+					# changing the note length would require the measure to be fixed
+					new_note.midi_num = math.floor(random.gauss(note.midi_num, Measure.midi_number_stdev(measure)))
+				new_notes.append(new_note)
+			new_gene.append(Measure(new_notes))
+		result.append(new_gene)
+	return result
 
 
 def run(options):
-    num_measures = options['midi']['measures']
-    num_notes = num_measures * 16
-    num_iterations = options['genetic']['iterations']
-    crossover_percentage = options['genetic']['crossover']
-    mutation_percentage = options['genetic']['mutation']
-    start_note = 60
-    scale = options['midi']['scale']
-	# TODO: chang ethe way initial population length is calculated
-    num_initial_population = int(num_notes * (options['genetic']['crossover'] ** -options['genetic']['iterations']))
-    debug_print("Calculated initial population: {}".format(num_initial_population))
-    pop = initial_population(num_initial_population)
-    favor_array = [1, 1, 1.5, 1, 2, 1, 1]
-    favor_gain = 0.25
-    for i in range(num_iterations):
-        selected_pop = selection(pop, start_note, scale, favor_array, favor_gain)
-        next_gen = crossover(selected_pop, crossover_percentage)
-        pop = mutate(next_gen, mutation_percentage)
-        debug_print('[')
-        for item in pop:
-            debug_print(format(item, '#011b'))
-        debug_print(']')
-        if len(pop) <= num_notes:
-            break
-    return pop
+	# num_measures = options['midi']['measures']
+	num_iterations = options['genetic']['iterations']
+	crossover_percentage = options['genetic']['crossover']
+	mutation_percentage = options['genetic']['mutation']
+	#scale = options['midi']['scale']
+	pop = initial_population(options['genetic']['population'])
+	for i in range(num_iterations):
+		if len(pop) == 1:
+			break
+		selected_pop = selection(pop)
+		next_gen = selected_pop
+		if len(selected_pop) > 1:
+			next_gen = crossover(selected_pop, crossover_percentage)
+		pop = mutate(next_gen, mutation_percentage)
+	return pop
--- measure.py
+++ measure.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 import math
 import random
 import statistics
+import bisect
 
 
 class Note:
@@ -10,31 +11,22 @@ class Note:
 	decimal_two = Decimal(2)
 
 
-	def __init__(self, note_num=60, note_len=Decimal(0.25), dotted=False, is_rest=False):
+	def __init__(self, note_num=60, note_len=Decimal(0.25), is_rest=False):
 		self.midi_num = note_num
 		self.note_len = Decimal(note_len)
-		self.dotted = dotted
 		self.is_rest = is_rest
 
 
 	# checks if the note has a valid length
-	# returns true if log_2(note_length) is a nonpositive integer
+	# returns true if the note is a multiple of a nonpositive power of two
 	def is_valid_length(self):
-		log = self.note_len.log10() / Note.decimal_two.log10()
-		return log <= 0 and math.ceil(log) == math.floor(log)
-
-
-	# calculate the note's length, taking into account if the note is dotted
-	def actual_length(self):
-		multiplier = 1
-		if self.dotted:
-			multiplier = 1.5
-		return self.note_len * multiplier
+		numer, denom = map(Decimal, self.note_len.as_integer_ratio())
+		log = denom.log10() / Note.decimal_two.log10()
+		return math.ceil(log) == math.floor(log) and numer <= denom
 
 
 	def __str__(self):
-		return "(Note number = {}, length = {}, is_rest = {}, dotted = {})".format(self.midi_num,
-			self.note_len, self.is_rest, self.dotted)
+		return "(Note number = {}, length = {}, is_rest = {})".format(self.midi_num,self.note_len, self.is_rest)
 
 
 class Measure:
@@ -43,31 +35,54 @@ class Measure:
 
 
 	def __str__(self):
-		return str(self.notes)
+		return '[' + ', '.join(str(note) for note in self.notes) + ']'
+
+
+	def total_note_length(self):
+		return sum(map(lambda note: note.note_len, self.notes))
 
 
 	# checks if the measure is a valid sequence of notes
 	# for now we default to 4/4
-	# measure is valid if each note is valid and if the note lengths
-	# add up to 1
+	# measure is valid if the total length of the notes is 1	
 	def check_valid(self):
-		sum_lengths = 0
-		for note in self.notes:
-			if not note.is_valid_length():
-				return False
-			sum_lengths += note.actual_length()
-		return sum_lengths == 1
+		return self.total_note_length() == 1
 
 
 	def split(self, cutoff_point):
 		running_total = 0
 		current_index = 0
 		while running_total < cutoff_point:
-			running_total += self.notes[current_index].actual_length()
+			running_total += self.notes[current_index].note_len
 			current_index += 1
 		if running_total > cutoff_point:
 			current_index -= 1
 		return (self.notes[0: current_index], self.notes[current_index:])
+
+
+	# fix note lengths so that the measure is valid
+	def fix(self):
+		total_length = self.total_note_length()
+		if total_length == 1:
+			return
+		new_notes = []			
+		new_length = 0
+		current_index = 0
+		while new_length < 1 and current_index < len(self.notes):
+			next_sum = new_length + self.notes[current_index].note_len
+			if next_sum <= 1:
+				new_length = next_sum
+				new_notes.append(self.notes[current_index])
+				current_index += 1
+			else:
+				# this is necessary for the cases where the last note in the list
+				# would go over 1, causing an infinite loop
+				break
+		if new_length < 1:
+			diff = 1 - new_length
+			new_notes[-1].note_len += diff
+		self.notes = new_notes
+				
 
 
 	# most basic possible random measure generation
@@ -92,18 +107,22 @@ class Measure:
 	# to make it easier to add them to lists of measures for the fitness function
 	# calculate the percentage of the measure that are rests
 	def percent_vacant(measure):
-		return sum(map(lambda note: note.actual_length(), filter(lambda note: note.is_rest, measure.notes)))
+		return sum(map(lambda note: note.note_len, filter(lambda note: note.is_rest, measure.notes)))
 
 
 	def note_length_stdev(measure):
-		return statistics.stdev(map(lambda note: note.actual_length(), measure.notes))
+		if len(measure.notes) <= 1:
+			return 0
+		return statistics.stdev(map(lambda note: note.note_len, measure.notes))
 
 
 	def midi_number_stdev(measure):
+		if len(measure.notes) <= 1:
+			return 0
 		return statistics.stdev(map(lambda note: note.midi_num, measure.notes))
 
 
 	# TODO: include measure for presence of patterns/motifs in the measure
 	DEFAULT_MEASUREMENTS = [percent_vacant, note_length_stdev, midi_number_stdev]
-	DEFAULT_TARGETS = [0.0625, 0.125, 2]
+	DEFAULT_TARGETS = [Decimal(0.0625), Decimal(0.125), Decimal(2)]
 
